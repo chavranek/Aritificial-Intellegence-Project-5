0c4c39713ac149054e6806e1dd82175a2fc7ff49
chavranek <havranek@sonoma.edu>
2018-11-15 18:19:52 -0800

    load images from mnist

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from mnist import MNIST
+
 def main():
     '''main function that will do things'''
-    print("Hello World")
+    mndata = MNIST('samples')
+
+
+    #loading the images from mnist
+    trainImages, trainLabels = mndata.load_training()
+
+    testImages, testLabels = mndata.load_testing()
 
 
 if __name__ == "__main__":
