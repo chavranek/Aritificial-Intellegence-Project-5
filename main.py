b0907db79336933454ef81542ea64f590a7656ff
chavranek <havranek@sonoma.edu>
2018-11-16 18:33:11 -0800

    specific images and labels function

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
+from PIL import Image
 from mnist import MNIST
 import random
 
-def getLabelIndicees(labels, cls1, cls2):
+def specificDigits(labels,images, cls1, cls2):
     '''this function will grab the two desired image classes and output the
     new corresponding label and image arrays
-    ------------------WARNING---------------------
-    not finished yet
     '''
 
     specLabels = []
     specImages = []
     for i in range(len(labels)):
         if labels[i] == cls1 or labels[i] == cls2:
-            specLabels.append(i)
+            specLabels.append(labels[i])
+            specImages.append(images[i])
 
     return specLabels, specImages
 
 def main():
-    '''main function that will do things'''
-    #christians branch
+    '''main function'''
 
     mndata = MNIST('samples')
 
@@ -28,9 +27,12 @@ def main():
 
     testImages, testLabels = mndata.load_testing()
 
-    index = random.randrange(0, len(testImages))  # choose an index ;-)
-    print(mndata.display(testImages[index]))
+    onesAndFivesLabels, onesAndFivesImages = specificDigits(trainLabels, trainImages, 1, 5)
 
+    index = random.randrange(0, len(onesAndFivesLabels))  # choose an index ;-)
+    print(mndata.display(onesAndFivesImages[index]))
+
+    print(onesAndFivesLabels[index])
 
 if __name__ == "__main__":
     main()
