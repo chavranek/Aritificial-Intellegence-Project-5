92ceadbd3d46ee945cf6044ab747672a763a84b1
chavranek <havranek@sonoma.edu>
2018-11-16 16:16:01 -0800

    getting new arrays for specific classes

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from mnist import MNIST
 import random
 
+def getLabelIndicees(labels, cls1, cls2):
+    '''this function will grab the two desired image classes and output the
+    new corresponding label and image arrays
+    ------------------WARNING---------------------
+    not finished yet
+    '''
+
+    specLabels = []
+    specImages = []
+    for i in range(len(labels)):
+        if labels[i] == cls1 or labels[i] == cls2:
+            specLabels.append(i)
+
+    return specLabels, specImages
+
 def main():
     '''main function that will do things'''
     #christians branch
