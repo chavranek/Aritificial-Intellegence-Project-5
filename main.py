f8233a93df4d218e6778c7ac01addbc844897c0c
chavranek <havranek@sonoma.edu>
2018-11-16 19:01:45 -0800

    def to gray works with downloaded mnist image

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-from PIL import Image
+#from PIL import Image
 from mnist import MNIST
 import random
+
+'''def to_grey(image):
+    im = image.open
+    width, height = im.size()
+    pixels = im.load()
+    pixels = image
+
+    for i in range(0, width):
+        for j in range(0, height):
+            if(pixels[i][j] >= 128):
+                pixels[i][j] == 0
+
+            if(pixels[i][j] <= 128):
+                pixels[i][j] == 1
+    return pixels'''
+
+def to_grey(greyImage):
+    '''images loaded in from the mnist data set are already lists
+        so we can just iterate through it like a list'''
+    for i in range(0, len(greyImage)):
+        if greyImage[i] >= 128:
+            greyImage[i] = 1
+
+        elif greyImage[i] < 128:
+            greyImage[i] = 0
+
+    print(len(greyImage))
+    return greyImage
+
+'''def symmetry_dens(image):
+
+    im = Image.open(image)
+
+    # open reverse
+    #rev_im = Image.open(image)'''
 
 def specificDigits(labels,images, cls1, cls2):
     '''this function will grab the two desired image classes and output the
@@ -33,6 +68,12 @@ def main():
     print(mndata.display(onesAndFivesImages[index]))
 
     print(onesAndFivesLabels[index])
+
+    image2 = onesAndFivesImages[0]
+
+    image2 = to_grey(image2)
+
+    print(image2)
 
 if __name__ == "__main__":
     main()
