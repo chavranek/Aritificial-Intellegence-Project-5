4203b86d28074baa9ba20d959d5b5ea1e982aa9d
chavranek <havranek@sonoma.edu>
2018-11-19 13:45:12 -0800

    grey image thresh hold modified. symmetry_density works with lists and only evaluates where a 1 shows up

--- main.py
+++ main.py
@@ -1,46 +1,57 @@
-#from PIL import Image
 from mnist import MNIST
 import random
 
-'''def to_grey(image):
-    im = image.open
-    width, height = im.size()
-    pixels = im.load()
-    pixels = image
 
-    for i in range(0, width):
-        for j in range(0, height):
-            if(pixels[i][j] >= 128):
-                pixels[i][j] == 0
+def density(image):
+    # takes the non modified image with the original
+    # pixel values in it
+    return sum(image) / len(image)
 
-            if(pixels[i][j] <= 128):
-                pixels[i][j] == 1
-    return pixels'''
 
-def to_grey(greyImage):
-    '''images loaded in from the mnist data set are already lists
-        so we can just iterate through it like a list'''
-    for i in range(0, len(greyImage)):
-        if greyImage[i] >= 128:
+def symmetry_density(image):
+    # reverse the image list and do an XOR comparison
+    # on every value in the array. every time the value
+    # returned from the XOR is a 0 that means there is
+    # a same pixel value and it gets added to the x value to
+    # be averaged out once the list has been exhausted
+
+    reflected = list(reversed(image))
+    x = 0
+
+    numOnes = sum(image)
+
+    for i in range(0, len(image)):
+        # want to add a 1 for every same value found
+        # disregarding all 0 0 pairs
+        if image[i] == 1 or reflected[i] == 1:
+            if not (image[i] ^ reflected[i]):
+                x += 1
+
+    return x / numOnes
+
+
+def to_grey(image):
+
+    # images loaded in from the mnist data set are already lists
+    # so we can just iterate through it like a list changing all values
+    # greater than 0 to 1 and all values equal to 0 stay zero.
+    # we use 0 as the threshold because any value over 0 means there is a
+    # mark that is a part of the number even if it is lighter
+    greyImage = image.copy()
+    for i in range(0, len(image)):
+        if image[i] > 0:
             greyImage[i] = 1
 
-        elif greyImage[i] < 128:
+        else:
             greyImage[i] = 0
 
-    print(len(greyImage))
     return greyImage
 
-'''def symmetry_dens(image):
-
-    im = Image.open(image)
-
-    # open reverse
-    #rev_im = Image.open(image)'''
 
 def specificDigits(labels,images, cls1, cls2):
-    '''this function will grab the two desired image classes and output the
-    new corresponding label and image arrays
-    '''
+
+    # this function will grab the two desired image classes and output the
+    # new corresponding label and image arrays
 
     specLabels = []
     specImages = []
@@ -51,8 +62,8 @@ def specificDigits(labels,images, cls1, cls2):
 
     return specLabels, specImages
 
+
 def main():
-    '''main function'''
 
     mndata = MNIST('samples')
 
@@ -67,13 +78,30 @@ def main():
     index = random.randrange(0, len(onesAndFivesLabels))  # choose an index ;-)
     print(mndata.display(onesAndFivesImages[index]))
 
-    print(onesAndFivesLabels[index])
+    imageCopy1 = onesAndFivesImages[index].copy()
 
-    image2 = onesAndFivesImages[0]
+    imageCopy2 = imageCopy1.copy()
 
-    image2 = to_grey(image2)
+    greyImage = to_grey(imageCopy1)
 
-    print(image2)
+    symmetryAverage = symmetry_density(greyImage)
+
+    densityImage = density(imageCopy2)
+
+    print("Image List Representation = ", imageCopy1)
+
+    print("Image Class =", onesAndFivesLabels[index])
+
+    print("Image Index =", index)
+
+    print("Grey Image =", greyImage)
+
+    print("Symmetry Average =", symmetryAverage)
+
+    print("Average Density =", densityImage)
+
+
+
 
 if __name__ == "__main__":
     main()
